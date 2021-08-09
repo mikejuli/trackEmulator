7fe74af1dddcbf05877d94eb5dfbdee647d09182
Mikhail <juliinstant88@gmail.com>
2021-08-08 21:33:57 -0700

    adding backward moving

--- truck.js
+++ truck.js
@@ -10,7 +10,7 @@ document.addEventListener ( 'keydown', function (event){
 
   if(event.key === 'a' || event.key === 'ArrowLeft'){
     console.log(event.key);
-    rotate-=5;
+    rotate-=3;
     document.getElementById('wheel-front-left').style.transform = 'rotate('+rotate+'deg)';
     document.getElementById('wheel-front-right').style.transform = 'rotate('+rotate+'deg)';
 
@@ -18,21 +18,33 @@ document.addEventListener ( 'keydown', function (event){
 
   if(event.key === 'd'|| event.key === 'ArrowRight'){
     console.log(event.key);
-    rotate+=5;
+    rotate+=3;
     document.getElementById('wheel-front-left').style.transform = 'rotate('+rotate+'deg)';
     document.getElementById('wheel-front-right').style.transform = 'rotate('+rotate+'deg)';
   }
 
   if(event.key === 'w'|| event.key === 'ArrowUp'){
-    tempRotate+=rotate;
+
+
+    // var it = setInterval(()=>{
+
+
+
+    //       console.log('here');
+    //       clearInterval(it);
+    //     }
+
+
+
+
+
+      tempRotate+=rotate;
+
+    positionY= positionY - (10 * Math.cos((tempRotate) * Math.PI/180));
+    positionX= positionX + (10 * Math.sin((tempRotate) * Math.PI/180));
+
     console.log(event.key, tempRotate);
-
     console.log( Math.sin(tempRotate * Math.PI/180));
-
-
-    positionY= positionY - (15 * Math.cos((tempRotate) * Math.PI/180));
-    positionX= positionX + (15 * Math.sin((tempRotate) * Math.PI/180));
-
     console.log(positionY,positionX);
 
     if(rotate!== 0 ){
@@ -42,6 +54,11 @@ document.addEventListener ( 'keydown', function (event){
 
     document.getElementById('truck').style.marginLeft =  positionX + 'px';
     document.getElementById('truck').style.marginTop =  positionY + 'px';
+
+
+  //    }, 50);
+
+
 
       }
 
@@ -53,10 +70,13 @@ document.addEventListener ( 'keydown', function (event){
     console.log(event.key);
 
     positionY= positionY + (15 * Math.cos((tempRotate) * Math.PI/180));
-    positionX= positionX - (15 * Math.sin((tempRotate) * Math.PI/180));
+    positionX= positionX + (15 * Math.sin((tempRotate) * Math.PI/180));
+
+    console.log(positionY, positionX);
+    console.log(15* Math.cos((tempRotate) * Math.PI/180),15* Math.sin((tempRotate) * Math.PI/180));
 
     if(rotate!== 0 ){
-      document.getElementById('truck').style.transform = 'rotate('+ tempRotate+'deg)';
+      document.getElementById('truck').style.transform = 'rotate('+ (-tempRotate)+'deg)';
     }
 
     document.getElementById('truck').style.marginLeft =  positionX + 'px';
